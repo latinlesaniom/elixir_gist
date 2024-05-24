237e665dc174b2a8b4a0cd7aeaa4f0ea1ec8bea1
Manuel Morales <latinchlesus@gmail.com>
2024-05-24 12:29:35 -0600

    first commit

---
+++ lib/elixir_gist_web/live/gist_live.ex
@@ -0,0 +1,29 @@
+defmodule ElixirGistWeb.GistLive do
+  use ElixirGistWeb, :live_view
+
+  alias ElixirGist.Gists
+  alias ElixirGistWeb.GistFormComponent
+
+  def mount(%{"id" => id}, _session, socket) do
+    gist = Gists.get_gist!(id)
+
+    {:ok, relative_time} = Timex.format(gist.updated_at, "{relative}", :relative)
+
+    gist = Map.put(gist, :relative, relative_time)
+
+    {:ok, assign(socket, gist: gist)}
+  end
+
+  def handle_event("delete", %{"id" => id}, socket) do
+    case Gists.delete_gist(socket.assigns.current_user, id) do
+      {:ok, _gist}  ->
+        socket = put_flash(socket, :info, "Gist Successfully deleted")
+        {:noreply, push_navigate(socket, to: ~p"/create")}
+
+
+        {:error, message} ->
+          socket = put_flash(socket, :error, message)
+          {:noreply, socket}
+      end
+  end
+end
